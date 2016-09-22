ad68bb5ef2d23d0ece874c9bcea46fad9738fd4b
James Green <jrgreen@gmail.com>
2016-09-22 22:59:49 +0100

    Update css

--- scripts/CVController.js
+++ scripts/CVController.js
@@ -1,9 +1,4 @@
 app.controller('CVController', ['$scope', function ($scope) {
     $scope.data = pageData;
-    $scope.status = {
-        isCustomHeaderOpen: false
-        , isFirstOpen: true
-        , isFirstDisabled: false
-    };
 
 }]);
