52cf1f0511d904823c74773ce3acc2d2106ab406
James Green <jrgreen@gmail.com>
2016-09-22 21:33:47 +0100

    Controller change for contact info

--- scripts/CVController.js
+++ scripts/CVController.js
@@ -1,5 +1,9 @@
 app.controller('CVController', ['$scope', function ($scope) {
     $scope.data = pageData;
-
+    $scope.status = {
+        isCustomHeaderOpen: false
+        , isFirstOpen: true
+        , isFirstDisabled: false
+    };
 
 }]);
